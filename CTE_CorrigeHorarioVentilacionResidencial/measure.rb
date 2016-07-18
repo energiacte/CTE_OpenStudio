d196703d516d1570acbf40e90e27676338164e13
Rafael Villar Burke <pachi@ietcc.csic.es>
2016-07-18 11:38:21 +0200

    CorrigeHorarioVentilacionResidencial: documenta mejor

--- CTE_CorrigeHorarioVentilacionResidencial/measure.rb
+++ CTE_CorrigeHorarioVentilacionResidencial/measure.rb
@@ -3,8 +3,26 @@
 CTE_SCHEDULE_NAME = "CTER24B_HVEN"
 
 class CTE_CorrigeHorarioVentilacionResidencial < OpenStudio::Ruleset::WorkspaceUserScript
-  # OpenStudio convierte los horarios de ventilación de los objetos ZoneVentilation:DesignFlowRate a Always_On a EPlus
-  # Esto sucede cuando se usa un objeto OutdoorAir, que es necesario cuando no se usan sistemas ideales
+  # 1 - Corrección de horarios de ventilación nocturna y caudal de diseño (HS3)
+  # OpenStudio genera un objeto ZoneVentilation:DesignFlowRate con horario Always_On cuando se introducen
+  # sistemas ideales. Puesto que usamos objetos ZoneVentilation:DesignFlowRate para introducir el caudal
+  # de diseño de aire de renovación (HS3) y la ventilación noctura, debemos cambiar el horario Always_On
+  # por uno que cuando se use con fracción 1 nos de 4 ren/h en horario nocturno de verano y el caudal
+  # de diseño el resto del tiempo (CTER24B_HVEN).
+  # En el caso de sistemas reales no se genera el objeto y lo debemos introducir en la medida.
+  # Los sistemas reales en principio usan un objeto OutDoorAir que se conecta a la caja de mezclas, de modo
+  # que se debe eliminar esta caja puesto que no realizaremos el control de aire exterior de ese modo.
+  # Nuestra plantilla define un objeto de aire exterior que indica las 4ren/h para pasar los parámetros a
+  # los sistemas ideales.
+
+  # 2 - Introducción de objetos de balance de aire exterior
+  # En lugar de una red de flujos, la consideración de las infiltraciones se está haciendo mediante
+  # el método ELA, (ZoneInfiltration:...), de forma desacoplada a la ventilación (ZoneVentilation:DesignFlowRate).
+  # Para tener en cuenta la interacción entre ambos componentes se usa el objeto de ZoneAirBalance:OutdoorAir,
+  # que realiza una combinación cuadrática de ambas componentes Q^2 = Q_v^2 + Q_i^2
+  # Al realizar este cambio, los resultados de aire exterior se muestran en variables separadas del tipo:
+  #  HVAC,Sum,Zone Combined Outdoor Air...
+
 
   def name
     return "Corrige horario de ventilacion a CTER24B_HVEN"
@@ -18,8 +36,10 @@ class CTE_CorrigeHorarioVentilacionResidencial < OpenStudio::Ruleset::WorkspaceU
   def run(workspace, runner, user_arguments)
     super(workspace, runner, user_arguments)
 
-    runner.registerInitialCondition("Cambio de horarios en todos los elementos ZoneVentilation_DesignFlowRate a #{ CTE_SCHEDULE_NAME }")
+    runner.registerInitialCondition("CTE: Ventilacion en uso residencial")
 
+    # **** 1 - Corrección de horarios de ventilación en objetos ZoneVentilation:DesignFlowRate es CTER24B_HVEN
+    runner.registerInfo("1 - Cambio de horarios en objetos ZoneVentilation_DesignFlowRate a #{ CTE_SCHEDULE_NAME }")
     idfObjects = workspace.getObjectsByType("ZoneVentilation_DesignFlowRate".to_IddObjectType)
     if idfObjects.empty?
       runner.registerInfo("No se han encontrado objetos ZoneVentilation_DesignFlowRate")
@@ -37,28 +57,22 @@ class CTE_CorrigeHorarioVentilacionResidencial < OpenStudio::Ruleset::WorkspaceU
         end
       end
     end
+    runner.registerInfo("Se han cambiado #{ changeCounter } horarios de #{ idfObjects.size } objetos ZoneVentilation_DesignFlowRate")
+    # ****
 
-    runner.registerInfo("Se han renombrado #{ changeCounter } de #{ idfObjects.size } objetos")
 
-    ### TODO: Esto necesita que se muestren los valores de Zone combined airflow ... en lugar de valores de zona en el report.
-    # SELECT AVG(data.VariableValue) FROM ReportVariableData AS data WHERE ReportVariableDataDictionaryIndex = 160;
-    # ReportVariableDataDictionary vale para buscar el Combined Air... que en este archivo era 160.
-
-    # ZoneAirBalance:OutdoorAir,
-    # LIVING ZONE Balance 1,   !- Name
-    # LIVING ZONE,             !- Zone Name
-    # Quadrature,              !- Air Balance Method
-    # 0.00,                    !- Induced Outdoor Air Due to Unbalanced Duct Leakage {m3/s}
-    # INF-SCHED;               !- Induced Outdoor Air Schedule Name
-
-    # array to hold new IDF objects needed
+    # -------------------------- Array de cadenas con objetos IDF
     string_objects = []
 
+
+    # **** 2 - Introducción de balance de aire exterior
+    runner.registerInfo("2 - Introducción de objetos ZoneAirBalance:OutdooAir")
     idfZones = workspace.getObjectsByType("Zone".to_IddObjectType)
     if not idfZones.empty?
+      runner.registerInfo("Localizado(s) #{ idfZones.size } objeto(s) Zone")
       idfZones.each do | idfZone |
         nombreZona = idfZone.getString(0)
-        runner.registerInfo("Añadiendo Objeto ZoneAirBalance:OutdoorAir a zona #{ nombreZona }")
+        runner.registerInfo("Definido objeto ZoneAirBalance:OutdoorAir a zona #{ nombreZona }")
 
         string_objects << "
           ZoneAirBalance:OutdoorAir,
@@ -69,10 +83,13 @@ class CTE_CorrigeHorarioVentilacionResidencial < OpenStudio::Ruleset::WorkspaceU
           CTER24B_HINF;             !- Induced Outdoor Air Schedule Name
           "
       end
+    else
+      runner.registerInfo("No se han encontrado objetos Zone")
     end
+    # ****
 
-    # add all of the strings to workspace
-    # this script won't behave well if added multiple times in the workflow. Need to address name conflicts
+
+    # -------------------------- Incorpora objetos definidos en cadenas al workspace (Debe hacerse de una vez)
     string_objects.each do |string_object|
       idfObject = OpenStudio::IdfObject::load(string_object)
       object = idfObject.get
