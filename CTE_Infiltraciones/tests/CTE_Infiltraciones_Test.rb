b32904ac4ae57bca1f238d1541dc3dc9c95b7fc0
Rafael Villar Burke <pachi@ietcc.csic.es>
2016-07-19 16:29:33 +0200

    CTE_infiltraciones: nuevo código refactorizado y con tests

--- CTE_Infiltraciones/tests/CTE_Infiltraciones_Test.rb
+++ CTE_Infiltraciones/tests/CTE_Infiltraciones_Test.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 require_relative '../measure.rb'
 require 'fileutils'
 
-class InfiltracionMetodoELATest < MiniTest::Unit::TestCase
+class CTE_InfiltracionesTest < MiniTest::Unit::TestCase
 
   # def setup
   # end
@@ -13,34 +13,24 @@ class InfiltracionMetodoELATest < MiniTest::Unit::TestCase
   # end
 
   def test_good_argument_values
-    # create an instance of the measure
-    measure = InfiltracionMetodoELA.new
-
-    # create an instance of a runner
+    measure = CTE_Infiltraciones.new
     runner = OpenStudio::Ruleset::OSRunner.new
 
     # load the test model
     translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/example_model.osm")
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/ejemploCTE.osm")
     model = translator.loadModel(path)
     assert((not model.empty?))
     model = model.get
 
-    # get arguments
+    # get arguments and load non default values
     arguments = measure.arguments(model)
     argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
 
-    # create hash of argument values.
-    # If the argument has a default that you want to use, you don't need it in the hash
     args_hash = {}
-    args_hash["zone"] = "Thermal Zone 1"
-    args_hash["vent_sch"] = "Medium Office Bldg Occ"
-    args_hash["design_flow_rate"] = 50.0
-    # using defaults values from measure.rb for other arguments
-
-    # populate argument with specified hash value if specified
+    args_hash["tipoEdificio"] = "Nuevo"
+    args_hash["permeabilidadVentanas"] = "Clase 2"
     arguments.each do |arg|
-      puts arg.name
       temp_arg_var = arg.clone
       if args_hash[arg.name]
         assert(temp_arg_var.setValue(args_hash[arg.name]))
@@ -51,8 +41,6 @@ class InfiltracionMetodoELATest < MiniTest::Unit::TestCase
     # run the measure
     measure.run(model, runner, argument_map)
     result = runner.result
-
-    # show the output
     show_output(result)
 
     # assert that it ran correctly
@@ -60,7 +48,26 @@ class InfiltracionMetodoELATest < MiniTest::Unit::TestCase
 
     # save the model to test output directory
     output_file_path = OpenStudio::Path.new(File.dirname(__FILE__) + "/output/test_output.osm")
-    model.save(output_file_path,true)
+    model.save(output_file_path, true)
+  end
+
+  def test_bad_model
+    measure = CTE_Infiltraciones.new
+    runner = OpenStudio::Ruleset::OSRunner.new
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/example_model.osm")
+    model = translator.loadModel(path)
+    assert((not model.empty?))
+    model = model.get
+
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Ruleset.convertOSArgumentVectorToMap(arguments)
+
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    show_output(result)
+
+    assert_equal("Fail", result.value.valueName)
   end
 
 end
