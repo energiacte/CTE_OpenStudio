9313de2fea462f950df89ba6d561856c78fd02fb
Daniel Jiménez González <danielj@ietcc.csic.es>
2016-10-10 11:54:23 +0200

    CTE_Model: paso de las variables a mensual

--- CTE_Model/resources/cte_lib_measures_addvars.rb
+++ CTE_Model/resources/cte_lib_measures_addvars.rb
@@ -75,19 +75,23 @@ def cte_addvars(model, runner, user_arguments)
     ["Zone Ideal Loads Zone Total Cooling Energy", "monthly", "*"],
     #["Zone Ideal Loads Outdoor Air Standard Density Volume Flow Rate", "monthly", "*"],
     #["Zone Ideal Loads Supply Air Standard Density Volume Flow Rate", "monthly", "*"],
+    ["Zone Combined Outdoor Air Sensible Heat Loss Energy", "monthly", "*"],
+    ["Zone Combined Outdoor Air Sensible Heat Gain Energy", "monthly", "*"],
+    ["Zone Combined Outdoor Air Changes per Hour", "monthly", "*"]
+    
     # Hourly variables
-    ["Surface Inside Face Conduction Heat Transfer Energy", "hourly", "*"],
-    ["Surface Window Heat Gain Energy", "hourly", "*"],
-    ["Surface Window Heat Loss Energy", "hourly", "*"],
-    ["Surface Window Transmitted Solar Radiation Energy", "hourly", "*"],
-    ["Zone Thermostat Cooling Setpoint Temperature", "hourly", "*"],
-    ["Zone Thermostat Heating Setpoint Temperature", "hourly", "*"],
-    ["Zone Total Internal Total Heating Energy", "hourly", "*"],
+    #~ ["Surface Inside Face Conduction Heat Transfer Energy", "hourly", "*"],
+    #~ ["Surface Window Heat Gain Energy", "hourly", "*"],
+    #~ ["Surface Window Heat Loss Energy", "hourly", "*"],
+    #~ ["Surface Window Transmitted Solar Radiation Energy", "hourly", "*"],
+    #~ ["Zone Thermostat Cooling Setpoint Temperature", "hourly", "*"],
+    #~ ["Zone Thermostat Heating Setpoint Temperature", "hourly", "*"],
+    #~ ["Zone Total Internal Total Heating Energy", "hourly", "*"],
     #["Zone Total Internal Convective Heating Energy", "hourly", "*"], # parte convectiva de la carga total de la zona
-    ["Zone Mechanical Ventilation Current Density Volume", "hourly", "*"],
-    ["Zone Combined Outdoor Air Sensible Heat Loss Energy", "hourly", "*"],
-    ["Zone Combined Outdoor Air Sensible Heat Gain Energy", "hourly", "*"],
-    ["Zone Combined Outdoor Air Changes per Hour", "hourly", "*"]
+    #~ ["Zone Mechanical Ventilation Current Density Volume", "hourly", "*"],
+    #~ ["Zone Combined Outdoor Air Sensible Heat Loss Energy", "hourly", "*"],
+    #~ ["Zone Combined Outdoor Air Sensible Heat Gain Energy", "hourly", "*"],
+    #~ ["Zone Combined Outdoor Air Changes per Hour", "hourly", "*"]
   ]
 
   new_oputput_variables.each do | variable_name, reporting_frequency, key |
