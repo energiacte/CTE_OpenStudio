68bd679c3dceeea3218e15b582939e60b1de303f
Daniel Jiménez González <danielj@ietcc.csic.es>
2016-10-14 15:48:55 +0200

    CTE_Workspace: eliminados los recuperadores de calor en ZoneHVAC:IdealLoadAirSystem

--- CTE_Workspace/measure.rb
+++ CTE_Workspace/measure.rb
@@ -29,7 +29,6 @@ require_relative "resources/cte_lib_measures_ventresidencial.rb"
 require_relative "resources/cte_lib_measures_zoneairbalance.rb"
 require_relative "resources/cte_lib_measures_groundtemperature.rb"
 require_relative "resources/cte_lib_measures_horarioestacional.rb"
-require_relative "resources/cte_lib_measures_recuperadorcalor.rb"
 
 # Medida de OpenStudio (WorkspaceUserScript) que modifica el modelo de EnergyPlus para uso con el CTE
 # Esta medida se aplica a modelos transformados por medidas de modelo CTE
@@ -51,30 +50,6 @@ class CTE_Workspace < OpenStudio::Ruleset::WorkspaceUserScript
   def arguments(workspace)
     args = OpenStudio::Ruleset::OSArgumentVector.new
 
-    # Heat Recovery Type
-    recuperador_chs = OpenStudio::StringVector.new
-    recuperador_chs << 'Ninguno'
-    recuperador_chs << 'Sensible'
-    recuperador_chs << 'Entálpico'
-    recuperador = OpenStudio::Ruleset::OSArgument::makeChoiceArgument('recuperador', recuperador_chs, true)
-    recuperador.setDisplayName("Recuperador de calor")
-    recuperador.setDefaultValue('Ninguno')
-    args << recuperador
-
-    #Sensible Heat Recovery Effectiveness
-    sensible_effectiveness = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("sensible_effectiveness", true)
-    sensible_effectiveness.setDisplayName("efectividad de la recuperación sensible")
-    sensible_effectiveness.setUnits("")
-    sensible_effectiveness.setDefaultValue(0.7)
-    args << sensible_effectiveness
-
-    #Latent Heat Recovery Effectiveness
-    latente_effectiveness = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("latente_effectiveness", true)
-    latente_effectiveness.setDisplayName("efectividad de la recuperación latente")
-    latente_effectiveness.setUnits("")
-    latente_effectiveness.setDefaultValue(0.65)
-    args << latente_effectiveness
-
     return args
   end
 
@@ -93,7 +68,7 @@ class CTE_Workspace < OpenStudio::Ruleset::WorkspaceUserScript
       return false
     end
 
-    num_part = '6'
+    num_part = '5'
 
     if es_residencial
       runner.registerInfo("[1/#{num_part}] - Corrección de horarios de ventilación en objetos
@@ -123,12 +98,6 @@ class CTE_Workspace < OpenStudio::Ruleset::WorkspaceUserScript
     runner.registerInfo("[5/#{num_part}] - Introduce el cambio de hora los últimos domingos de marzo y octubre")
     result = cte_horarioestacional(runner, workspace)
     return result unless result == true
-
-
-    runner.registerInfo("[6/#{num_part}] - Introduce, en su caso, los recuperadores de calor")
-    result = cte_recuperadorcalor(runner, workspace, user_arguments)
-    return result unless result == true
-
 
     return true
   end
