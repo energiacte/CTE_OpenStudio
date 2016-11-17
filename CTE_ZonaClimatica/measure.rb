7551940376abfa5e9d783fcdbab0776920c58d0e
Rafael Villar Burke <pachi@ietcc.csic.es>
2016-11-17 12:14:06 +0100

    CTE_ZonaClimatica: arregla y usa solamnente un desplegable

--- CTE_ZonaClimatica/measure.rb
+++ CTE_ZonaClimatica/measure.rb
@@ -1,13 +1,8 @@
-#see the URL below for information on how to write OpenStudio measures
-# TODO: Remove this link and replace with the wiki
-# http://openstudio.nrel.gov/openstudio-measure-writing-guide
-
-#see the URL below for access to C++ documentation on model objects (click on "model" in the main window to view model objects)
-# http://openstudio.nrel.gov/sites/openstudio.nrel.gov/files/nv_data/cpp_documentation_it/model/html/namespaces.html
-
-# Author: Nicholas Long
-# Simple measure to load the EPW file and DDY file
-#~ require_relative 'resources/stat_file'
+# Author(s): Daniel Jiménez González, Rafael Villar Burke
+# email: pachi@ietcc.csic.es
+#
+# Measure based on previous measure by Nicholas Long
+# Simple measure to load the EPW file and DDY file for a given CTE climate name
 require_relative 'resources/epw'
 
 class CTE_ZonaClimatica < OpenStudio::Ruleset::ModelUserScript
@@ -17,6 +12,17 @@ class CTE_ZonaClimatica < OpenStudio::Ruleset::ModelUserScript
   def initialize
     super
     @weather_directory = File.expand_path(File.join(File.dirname(__FILE__), "./resources"))
+    @climatenames = ['A3_peninsula', 'A4_peninsula',
+                     'B3_peninsula', 'B4_peninsula',
+                     'C1_peninsula', 'C2_peninsula', 'C3_peninsula', 'C4_peninsula',
+                     'D1_peninsula', 'D2_peninsula', 'D3_peninsula',
+                     'E1_peninsula',
+                     'alpha1_canarias', 'alpha2_canarias', 'alpha3_canarias', 'alpha4_canarias',
+                     'A1_canarias', 'A2_canarias', 'A3_canarias', 'A4_canarias',
+                     'B1_canarias', 'B2_canarias', 'B3_canarias', 'B4_canarias',
+                     'C1_canarias', 'C2_canarias', 'C3_canarias', 'C4_canarias',
+                     'D1_canarias', 'D2_canarias', 'D3_canarias',
+                     'E1_canarias']
   end
 
   def name
@@ -26,31 +32,12 @@ class CTE_ZonaClimatica < OpenStudio::Ruleset::ModelUserScript
   def arguments(model)
     args = OpenStudio::Ruleset::OSArgumentVector.new
 
-    choices_zci = OpenStudio::StringVector.new
-    choices_zci << "alpha"
-    choices_zci << "A"
-    choices_zci << "B"
-    choices_zci << "C"
-    choices_zci << "D"
-    choices_zci << "E"
-    zona_climatica_invierno =  OpenStudio::Ruleset::OSArgument.makeChoiceArgument('zona_climatica_invierno', choices_zci, true)
-    zona_climatica_invierno.setDisplayName("Zona Climática de Invierno")
-    args << zona_climatica_invierno
-
-    choices_zcv = OpenStudio::StringVector.new
-    choices_zcv << "1"
-    choices_zcv << "2"
-    choices_zcv << "3"
-    choices_zcv << "4"
-    zona_climatica_verano =  OpenStudio::Ruleset::OSArgument.makeChoiceArgument('zona_climatica_verano', choices_zcv, true)
-    zona_climatica_verano.setDisplayName("Zona Climática de Verano")
-    args << zona_climatica_verano
-
-    peninsula = OpenStudio::Ruleset::OSArgument.makeBoolArgument('peninsular', true)
-    peninsula.setDisplayName("¿Es peninsular?")
-    peninsula.setDescription("Marque esta opción si la localidad de proyecto está en la península o Baleares")
-    peninsula.setDefaultValue(true)
-    args << peninsula
+    choices_climas = OpenStudio::StringVector.new
+    @climatenames.each { |climaname| choices_climas << climaname }
+    clima =  OpenStudio::Ruleset::OSArgument.makeChoiceArgument('zona_climatica', choices_climas, true)
+    clima.setDisplayName("Zona Climatica")
+    clima.setDefaultValue("D3_peninsula")
+    args << clima
 
     args
   end
@@ -62,22 +49,10 @@ class CTE_ZonaClimatica < OpenStudio::Ruleset::ModelUserScript
       return false
     end
 
-    zci = runner.getStringArgumentValue("zona_climatica_invierno", user_arguments)
-    zcv = runner.getStringArgumentValue("zona_climatica_verano", user_arguments)
-    penin = runner.getStringArgumentValue("peninsular", user_arguments)
+    clima_name = runner.getStringArgumentValue("zona_climatica", user_arguments)
+    weather_file = File.join(@weather_directory, clima_name + '.epw')
+    runner.registerInfo("weather file = #{ weather_file }")
 
-    weather_file_name = ''
-    weather_file_name << zci + zcv + '_'
-    peninsular = (penin == 'false') ? 'canarias' : 'peninsula'
-    weather_file_name << peninsular
-
-    runner.registerInfo("weather file = #{weather_file_name}")    
-    
-    unless (Pathname.new @weather_directory).absolute?
-      @weather_directory = File.expand_path(File.join(File.dirname(__FILE__), @weather_directory))
-    end
-
-    weather_file = File.join(@weather_directory, weather_file_name + '.epw')
     # Parse the EPW manually because OpenStudio can't handle multiyear weather files (or DATA PERIODS with YEARS)
     epw_file = OpenStudio::Weather::Epw.load(weather_file)
 
@@ -101,30 +76,31 @@ class CTE_ZonaClimatica < OpenStudio::Ruleset::ModelUserScript
 
     # Add or update site data
     site = model.getSite
-    site.setName(weather_file_name + '.epw')
+    site.setName(clima_name + '.epw')
     site.setLatitude(weather_lat)
     site.setLongitude(weather_lon)
     site.setTimeZone(weather_time)
     site.setElevation(weather_elev)
 
-    ddy_file_name = weather_file_name + '.ddy'
-    ddy_file = File.join(@weather_directory, ddy_file_name)
+    # Remove existing design days
+    model.getDesignDays.each do |d|
+        model.removeObject(d.handle)
+    end
+
+    # Add new design days
+    ddy_file = File.join(@weather_directory, clima_name + '.ddy')
     ddy_model = OpenStudio::EnergyPlus.loadAndTranslateIdf(ddy_file).get
     ddy_model.getObjectsByType("OS:SizingPeriod:DesignDay".to_IddObjectType).each do |d|
-      # grab only the ones that matter
-      ddy_list = /(Htg 99.6. Condns DB)|(Clg .4. Condns WB=>MDB)|(Clg .4% Condns DB=>MWB)/
+      ddy_list = /(Winter 0.4% Dry Bulb Temperature)|(Summer 99.6% Dry Bulb Temperature)/
       if d.name.get =~ ddy_list
         runner.registerInfo("Adding object #{d.name}")
-
-        # add the object to the existing model
         model.addObject(d.clone)
       end
     end
 
-    # todo - add final condition
     runner.registerFinalCondition("The final weather file is #{model.getWeatherFile.city} and the model has #{model.getDesignDays.size} design day objects")
 
-    true
+    return true
   end
 end
 
--- CTE_ZonaClimatica/tests/cte_ZonaClimatica_test.rb
+++ CTE_ZonaClimatica/tests/cte_ZonaClimatica_test.rb
@@ -13,7 +13,7 @@ class CTE_ZonaClimatica_Test < MiniTest::Unit::TestCase
     FileUtils.rm_f test_out_file if File.exist? test_out_file
 
     #test_new_weather_file = 'another_weather_file.epw'
-    test_new_weather_file = 'USA_MA_Boston-Logan.Intl.AP.725090_TMY3.epw'
+    test_new_weather_file = 'D3_peninsula.epw'
 
     # create an instance of the measure
     measure = CTE_ZonaClimatica.new
@@ -32,26 +32,18 @@ class CTE_ZonaClimatica_Test < MiniTest::Unit::TestCase
     if model.weatherFile.empty?
       puts "No weather file in current model"
     else
-      puts "Current weather file is #{model.weatherFile}"# unless model.weatherFile.empty?
+      puts "Current weather file is #{ File.basename(model.weatherFile.get.path.get.to_s) }"# unless model.weatherFile.empty?
     end
 
     arguments = measure.arguments(model)
-    assert_equal(3, arguments.size)
+    assert_equal(1, arguments.size)
 
     argument_map = OpenStudio::Ruleset::OSArgumentMap.new
 
     count = -1
     arg = arguments[count += 1].clone
-    assert(arg.setValue(File.dirname(__FILE__)))
-    argument_map["weather_directory"] = arg
-
-    arg = arguments[count += 1].clone
-    assert(arg.setValue(test_new_weather_file))
-    argument_map["weather_file_name"] = arg
-
-    arg = arguments[count += 1].clone
-    assert(arg.setValue('5B'))
-    argument_map["climate_zone"] = arg
+    assert(arg.setValue("D3_peninsula"))
+    argument_map["zona_climatica"] = arg
 
     measure.run(model, runner, argument_map)
     result = runner.result
@@ -60,151 +52,18 @@ class CTE_ZonaClimatica_Test < MiniTest::Unit::TestCase
     assert(result.warnings.size == 0, "Warnings are greater than 0")
     #assert(result.info.size == 2)
 
-    assert(model.getObjectsByType("OS:SizingPeriod:DesignDay".to_IddObjectType).count == 3, "Expected only 3 design day objects")
+    assert(model.getObjectsByType("OS:SizingPeriod:DesignDay".to_IddObjectType).count == 2, "Expected 2 design day objects")
 
-    puts "Final weather file is #{model.weatherFile.get}" unless model.weatherFile.empty?
-    puts "Final site data is #{model.getSite}" if model.getSite
-    puts "Final Water Mains Temp is #{model.getSiteWaterMainsTemperature}" if model.getSiteWaterMainsTemperature
+    puts "Final weather file is #{ File.basename(model.weatherFile.get.path.get.to_s) }" unless model.weatherFile.empty?
+    puts "Final site data is #{ model.getSite }" if model.getSite
+    puts "Final Water Mains Temp is #{ model.getSiteWaterMainsTemperature }" if model.getSiteWaterMainsTemperature
     model.save(test_out_file)
 
     assert(File.basename(model.weatherFile.get.path.get.to_s) == test_new_weather_file)
-    if test_new_weather_file =~ /Boston/
-      assert(model.getSite.latitude == 42.37)
-      assert(model.getSite.longitude == -71.02)
-    else
-      assert(model.getSite.latitude == 45)
-      assert(model.getSite.longitude == -45)
+    if test_new_weather_file =~ /D3/
+      assert(model.getSite.latitude == 40.68)
+      assert(model.getSite.longitude == -4.13)
     end
   end
 
-  def test_weather_file_WA_Renton
-    test_out_file = File.join(File.dirname(__FILE__), 'output', 'test_WA_Renton.osm')
-    FileUtils.rm_f test_out_file if File.exist? test_out_file
-
-    #test_new_weather_file = 'another_weather_file.epw'
-    test_new_weather_file = 'USA_WA_Renton.Muni.AP.727934_TMY3.epw'
-
-    # create an instance of the measure
-    measure = CTE_ZonaClimatica.new
-
-    # create an instance of a runner
-    runner = OpenStudio::Ruleset::OSRunner.new
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = File.join(File.dirname(__FILE__), "test.osm")
-    model = translator.loadModel(path)
-    assert((not model.empty?))
-    model = model.get
-
-    # convert this to measure attributes
-    if model.weatherFile.empty?
-      puts "No weather file in current model"
-    else
-      puts "Current weather file is #{model.weatherFile}"# unless model.weatherFile.empty?
-    end
-
-    arguments = measure.arguments(model)
-    assert_equal(3, arguments.size)
-
-    argument_map = OpenStudio::Ruleset::OSArgumentMap.new
-
-    count = -1
-    arg = arguments[count += 1].clone
-    assert(arg.setValue(File.dirname(__FILE__)))
-    argument_map["weather_directory"] = arg
-
-    arg = arguments[count += 1].clone
-    assert(arg.setValue(test_new_weather_file))
-    argument_map["weather_file_name"] = arg
-
-    arg = arguments[count += 1].clone
-    assert(arg.setValue('5B'))
-    argument_map["climate_zone"] = arg
-
-    measure.run(model, runner, argument_map)
-    result = runner.result
-    show_output(result)
-    assert(result.value.valueName == "Success")
-    assert(result.warnings.size == 0, "Warnings are greater than 0")
-    #assert(result.info.size == 2)
-
-    #assert(model.getObjectsByType("OS:SizingPeriod:DesignDay".to_IddObjectType).count == 3, "Expected only 3 design day objects")
-
-    puts "Final weather file is #{model.weatherFile.get}" unless model.weatherFile.empty?
-    puts "Final site data is #{model.getSite}" if model.getSite
-    puts "Final Water Mains Temp is #{model.getSiteWaterMainsTemperature}" if model.getSiteWaterMainsTemperature
-    model.save(test_out_file)
-
-    assert(File.basename(model.weatherFile.get.path.get.to_s) == test_new_weather_file)
-  end
-
-  def test_multiyear_weather_file
-    test_out_file = File.join(File.dirname(__FILE__), 'output', 'test_out_mulityear.osm')
-    FileUtils.rm_f test_out_file if File.exist? test_out_file
-
-    #test_new_weather_file = 'another_weather_file.epw'
-    test_new_weather_file = 'multiyear.epw'
-
-    # create an instance of the measure
-    measure = CTE_ZonaClimatica.new
-
-    # create an instance of a runner
-    runner = OpenStudio::Ruleset::OSRunner.new
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = File.join(File.dirname(__FILE__), "test.osm")
-    model = translator.loadModel(path)
-    assert((not model.empty?))
-    model = model.get
-
-    # convert this to measure attributes
-    if model.weatherFile.empty?
-      puts "No weather file in current model"
-    else
-      puts "Current weather file is #{model.weatherFile}"# unless model.weatherFile.empty?
-    end
-
-    arguments = measure.arguments(model)
-    assert_equal(3, arguments.size)
-
-    argument_map = OpenStudio::Ruleset::OSArgumentMap.new
-
-    count = -1
-    arg = arguments[count += 1].clone
-    assert(arg.setValue(File.dirname(__FILE__)))
-    argument_map["weather_directory"] = arg
-
-    arg = arguments[count += 1].clone
-    assert(arg.setValue(test_new_weather_file))
-    argument_map["weather_file_name"] = arg
-
-    arg = arguments[count += 1].clone
-    assert(arg.setValue('5B'))
-    argument_map["climate_zone"] = arg
-
-    measure.run(model, runner, argument_map)
-    result = runner.result
-    show_output(result)
-    assert(result.value.valueName == "Success")
-    assert(result.warnings.size == 0, "Warnings are greater than 0")
-    #assert(result.info.size == 2)
-
-    assert(model.getObjectsByType("OS:SizingPeriod:DesignDay".to_IddObjectType).count == 3, "Expected only 3 design day objects")
-
-    puts "Final weather file is #{model.weatherFile.get}" unless model.weatherFile.empty?
-    puts "Final site data is #{model.getSite}" if model.getSite
-    puts "Final Water Mains Temp is #{model.getSiteWaterMainsTemperature}" if model.getSiteWaterMainsTemperature
-    model.save(test_out_file)
-
-    assert(File.basename(model.weatherFile.get.path.get.to_s) == test_new_weather_file)
-    if test_new_weather_file =~ /Boston/
-      assert(model.getSite.latitude == 42.37)
-      assert(model.getSite.longitude == -71.02)
-    else
-      assert(model.getSite.latitude == 41.678)
-      assert(model.getSite.longitude == -70.937)
-    end
-  end
 end
