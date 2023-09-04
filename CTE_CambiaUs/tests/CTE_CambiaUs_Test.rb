6f757e8fb1c348dad18f50a5d5829aa328c242a2
Daniel Jiménez González <danielj@ietcc.csic.es>
2023-09-04 12:45:05 +0200

    usa paquetes de elementos de osm para los tests

--- CTE_CambiaUs/tests/CTE_CambiaUs_Test.rb
+++ CTE_CambiaUs/tests/CTE_CambiaUs_Test.rb
@@ -44,6 +44,20 @@ class CTE_CambiaUs_Test < MiniTest::Test
   #   # puts('Fin de setup')
   # end
 
+  def get_runner_model(file_path, measure)
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + file_path)
+    model = translator.loadModel(path)
+    assert((not model.empty?))
+    model = model.get
+
+    return runner, model
+  end
+
   def get_surface(model, uuid)
     handle = OpenStudio.toUUID(uuid)
     objeto = model.getModelObject(handle)
@@ -60,25 +74,32 @@ class CTE_CambiaUs_Test < MiniTest::Test
     return u
   end
 
-  def _test_CTE_CambiaUs_no_cambia
-    puts("\n____TEST::  CTE_CambiaUs_no_cambia")
+  def carga_elementos_residencial_osm()
+    #residencial OSM
+    muros_exteriores = ["7aeba99e-3d64-4b69-a3b2-a7cf318f91f8", "1415c487-a7ec-493b-b6e2-40cb1770a6d7", "41d017a5-0490-4aa0-81bf-cd5b5e4a86c2", "b82786ae-b6c9-4cc1-ace2-e2c1771d17b2"]
+    cubiertas_exteriores = ["7e5b0180-4940-4f82-8d55-a82b356ed256", "4dba6073-0b46-4a4e-88f5-9cf67b1c0b1e", "1b4202a7-389e-413c-a734-3903f64499f0", "c4875e32-9291-48b3-bfe0-94bad05eb0d5"]
+    cubiertas_interiores = ["bd6b3149-d34c-4ced-a718-7d9a506ca243", "d4a8d4b2-b59e-4e86-abe1-ba5a8fdc2431", "0355e260-8d73-4cd4-8b9c-f88882af2cad"]
+    suelos_exteriores = []
+    suelos_interiores = ["8c1e94b2-14cf-4a4c-9d46-907327050022", "50ded79a-3226-4fd9-98e9-e90968987d40", "57a5012c-8fa7-45a4-bd4e-05d1f80a1313"]
+    suelos_terrenos = ["ed02d7a6-7c4b-47a9-a072-0e7bf732a4d6", "1d1a27e5-c230-435a-ac39-f4210b362a6d", "134cfe13-6464-4e3f-8235-bc25a284eceb"]
+    # fixedWindows
+    ventanas = ["aedb937b-e035-4caf-8bd8-6d38aca58017", "bd75cc94-d2f4-4c61-89a0-ff79c0c406ba", "2f2adaad-60e6-4344-9d47-ee4787772d5c", "6852333b-05ee-4c91-a799-b7c1a0686274"]
+    # glass door
+    puertas = ["089b1b05-c16f-46c8-acf6-461e97125a7f"]
+    elementos = { "muros_exteriores" => muros_exteriores, "cubiertas_exteriores" => cubiertas_exteriores, "cubiertas_interiores" => cubiertas_interiores,
+    "suelos_interiores" => suelos_interiores, "suelos_terrenos" => suelos_terrenos}
+
+    return elementos
+  end
+
+  def test_CTE_CambiaUs_no_cambia
+    puts("\n------------------------------------------")
+    puts("____TEST::  CTE_CambiaUs_no_cambia______")
 
     # create an instance of the measure
     measure = CTE_CambiaUs.new
 
-    # create an instance of a runner
-    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/residencial.osm")
-    puts("cargando el modelo ", path)
-    model = translator.loadModel(path)
-    assert((not model.empty?))
-    model = model.get
-
-    # get arguments
-    puts("tomando los argumentos")
+    runner, model = get_runner_model("/residencial.osm", measure)
     arguments = measure.arguments(model)
     argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
 
@@ -87,7 +108,9 @@ class CTE_CambiaUs_Test < MiniTest::Test
     args_hash = {}
     args_hash["CTE_U_muros"] = 0
     args_hash["CTE_U_cubiertas"] = 0
-    # args_hash["CTE_U_suelos"] = 0.92
+    args_hash["CTE_U_suelos"] = 0
+    args_hash["CTE_U_huecos"] = 0
+
     # using defaults values from measure.rb for other arguments
 
     # populate argument with specified hash value if specified
@@ -99,36 +122,30 @@ class CTE_CambiaUs_Test < MiniTest::Test
       argument_map[arg.name] = temp_arg_var
     end
 
-    # Verifica que se modifica el modelo.
-    handle = OpenStudio.toUUID("cc187100-92a7-410b-bf38-3f6712910b74")
-    objeto = model.getModelObject(handle)
-    surface = objeto.get.to_Surface
-    surface = surface.get
-    construction = surface.construction.get
-    u_inicial = construction.thermalConductance().to_f
-    puts("|||Valores iniciales")
-    puts("|||cerramiento, #{surface.name}")
-    puts("|||construccion, #{construction.name}")
-    puts("|||U inicial del muro #{u_inicial}")
+    elementos_para_test = carga_elementos_residencial_osm()
 
-    muro = model.getModelObject(handle)
-    puts("ejecutando la medida")
+    # todos tienen que permanecer iguales
+    elementos = {}
+    elementos_para_test.each do |tipo, lista|
+      lista.each do |uuid|
+        u_inicial = get_transmitance(model, uuid)
+        elementos[uuid] = {"tipo" => tipo, "u_inicial" => u_inicial}
+      end
+    end
+    
+    puts("  ejecutando la medida")
     salida = measure.run(model, runner, argument_map)
     assert(salida, "algo falló")
 
-    handle = OpenStudio.toUUID("cc187100-92a7-410b-bf38-3f6712910b74")
-    objeto = model.getModelObject(handle)
-    surface = objeto.get.to_Surface
-    surface = surface.get
-    construction = surface.construction.get
-    u_final = construction.thermalConductance().to_f
-    puts("U inicial y final #{u_inicial}, #{u_final}")
-    puts("|||Valores finales")
-    puts("|||cerramiento, #{surface.name}")
-    puts("|||construccion, #{construction.name}")
-    puts("|||U final del muro #{u_final}")
-    assert_in_delta(u_inicial, u_final, 0.001)
+    elementos.each do |uuid, atributos|
+      u_final = get_transmitance(model, uuid)
+      atributos['u_final'] = u_final
+    end
 
+    elementos.each do |uuid, atr|
+      assert_in_delta(atr['u_inicial'], atr['u_final'], 0.001)
+    end
+    
     puts("___________ fin del test ________\n")
   end
 
@@ -281,20 +298,6 @@ class CTE_CambiaUs_Test < MiniTest::Test
     puts("__________ fin del test ________\n")
   end
 
-  def get_runner_model(file_path, measure)
-    # create an instance of a runner
-    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = OpenStudio::Path.new(File.dirname(__FILE__) + file_path)
-    model = translator.loadModel(path)
-    assert((not model.empty?))
-    model = model.get
-
-    return runner, model
-  end
-
   def _test_CTE_CambiaUs_cambia_suelos_residencial
     puts("\n____TEST:: CTE_CambiaUs_cambia_suelos")
 
@@ -436,7 +439,7 @@ class CTE_CambiaUs_Test < MiniTest::Test
     puts("__________ fin del test ________\n")
   end
 
-  def test_CTE_CambiaUs_extenso
+  def _test_CTE_CambiaUs_extenso
     puts("\n____TEST:: CTE_CambiaUs_cambia_suelos")
 
     # create an instance of the measure
@@ -454,12 +457,14 @@ class CTE_CambiaUs_Test < MiniTest::Test
     u_cubiertas = 0.3
     u_suelos = 0.40
     u_terrenos = 1 / (1 / u_suelos - 0.5)
+    u_huecos = 0.62
 
     args_hash = {}
     args_hash["CTE_U_muros"] = u_muros
     args_hash["CTE_U_cubiertas"] = u_cubiertas
     args_hash["CTE_U_suelos"] = u_suelos
-    
+    args_hash["CTE_U_huecos"] = u_huecos
+
     # using defaults values from measure.rb for other arguments
 
     # populate argument with specified hash value if specified
