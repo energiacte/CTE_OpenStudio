378b6002f708880451c0224a31e442d9a18e3214
Rafael Villar Burke <pachi@ietcc.csic.es>
2023-11-23 18:03:48 +0100

    Simplifica lectura temperaturas agua fría

--- CTE_Model/resources/cte_lib_measures_tempaguafria.rb
+++ CTE_Model/resources/cte_lib_measures_tempaguafria.rb
@@ -27,48 +27,62 @@ require "fileutils"
 
 CTE_HORARIOSAGUA = "CTE_ACS_Temperatura_agua_fria".freeze
 
-CAPITAL_Y_ALT_REF_FOR_CLIMATEZONE = {
-  "A3" => ["Cadiz", 0],
-  "A4" => ["Almeria", 0],
-  "B3" => ["Valencia", 8],
-  "B4" => ["Sevilla", 9],
-  "C1" => ["Bilbao_Bilbo", 214],
-  "C2" => ["Barcelona", 1],
-  "C3" => ["Granada", 754],
-  "C4" => ["Toledo", 445],
-  "D1" => ["Vitoria_Gasteiz", 512],
-  "D2" => ["Zamora", 617],
-  "D3" => ["Madrid", 589],
-  "E1" => ["Burgos", 861]
-}
-
 # Mapping de temperaturas de altitud y Tªs de agua fría de referencia por capital de provincia
-# TODO: Pasar a código y eliminar achivo
-# ["A_Coruna", "Albacete", "Alicante_Alacant", "Almeria", "Avila", "Badajoz", "Barcelona", "Bilbao_Bilbo",
-# "Burgos", "Caceres", "Cadiz", "Castellon_Castello", "Ceuta", "Ciudad_Real", "Cordoba", "Cuenca",
-# "Girona", "Granada", "Guadalajara", "Huelva", "Huesca", "Jaen", "Las_Palmas_de_Gran_Canaria", "Leon",
-# "Lleida", "Logrono", "Lugo", "Madrid", "Malaga", "Melilla", "Murcia", "Ourense", "Oviedo", "Palencia",
-# "Palma_de_Mallorca", "Pamplona_Iruna", "Pontevedra", "Salamanca", "San_Sebastian", "Santa_Cruz_de_Tenerife",
-# "Santander", "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo", "Valencia", "Valladolid",
-# "Vitoria_Gasteiz", "Zamora", "Zaragoza"]
-def cte_temps_map(runner)
-  temps_agua_file = File.dirname(__FILE__) + "/temperaturas_agua_fria.csv"
-  temps_agua_red = {}
-  File.read(temps_agua_file).each_line do |line|
-    next if line.start_with?("#")
-
-    csv_line = CSV.parse_line(line.strip, {col_sep: ","})
-    prov = csv_line[0].to_s
-    altref = csv_line[1].to_f
-    temps = csv_line[2..csv_line.size].map { |v| v.to_f }
-    temps_agua_red[prov] = [altref, temps]
-  rescue
-    runner.registerError("Error al leer archivo #{temps_agua_file} en línea #{line}")
-    return nil
-  end
-
-  temps_agua_red
-end
+# Provincia => [altitud_referencia,valores_mensuales_temperatura_agua_fria]
+CTE_TEMPS_MAP = {
+  "A_Coruna" => [0, [10, 10, 11, 12, 13, 14, 16, 16, 15, 14, 12, 11]],
+  "Albacete" => [677, [7, 8, 9, 11, 14, 17, 19, 19, 17, 13, 9, 7]],
+  "Alicante_Alacant" => [7, [11, 12, 13, 14, 16, 18, 20, 20, 19, 16, 13, 12]],
+  "Almeria" => [0, [12, 12, 13, 14, 16, 18, 20, 21, 19, 17, 14, 12]],
+  "Avila" => [1054, [6, 6, 7, 9, 11, 14, 17, 16, 14, 11, 8, 6]],
+  "Badajoz" => [168, [9, 10, 11, 13, 15, 18, 20, 20, 18, 15, 12, 9]],
+  "Barcelona" => [1, [9, 10, 11, 12, 14, 17, 19, 19, 17, 15, 12, 10]],
+  "Bilbao_Bilbo" => [214, [9, 10, 10, 11, 13, 15, 17, 17, 16, 14, 11, 10]],
+  "Burgos" => [861, [5, 6, 7, 9, 11, 13, 16, 16, 14, 11, 7, 6]],
+  "Caceres" => [385, [9, 10, 11, 12, 14, 18, 21, 20, 19, 15, 11, 9]],
+  "Cadiz" => [0, [12, 12, 13, 14, 16, 18, 19, 20, 19, 17, 14, 12]],
+  "Castellon_Castello" => [18, [10, 11, 12, 13, 15, 18, 19, 20, 18, 16, 12, 11]],
+  "Ceuta" => [0, [11, 11, 12, 13, 14, 16, 18, 18, 17, 15, 13, 12]],
+  "Ciudad_Real" => [630, [7, 8, 10, 11, 14, 17, 20, 20, 17, 13, 10, 7]],
+  "Cordoba" => [113, [10, 11, 12, 14, 16, 19, 21, 21, 19, 16, 12, 10]],
+  "Cuenca" => [975, [6, 7, 8, 10, 13, 16, 18, 18, 16, 12, 9, 7]],
+  "Girona" => [143, [8, 9, 10, 11, 14, 16, 19, 18, 17, 14, 10, 9]],
+  "Granada" => [754, [8, 9, 10, 12, 14, 17, 20, 19, 17, 14, 11, 8]],
+  "Guadalajara" => [708, [7, 8, 9, 11, 14, 17, 19, 19, 16, 13, 9, 7]],
+  "Huelva" => [50, [12, 12, 13, 14, 16, 18, 20, 20, 19, 17, 14, 12]],
+  "Huesca" => [432, [7, 8, 10, 11, 14, 16, 19, 18, 17, 13, 9, 7]],
+  "Jaen" => [436, [9, 10, 11, 13, 16, 19, 21, 21, 19, 15, 12, 9]],
+  "Las_Palmas_de_Gran_Canaria" => [114, [15, 15, 16, 16, 17, 18, 19, 19, 19, 18, 17, 16]],
+  "Leon" => [346, [6, 6, 8, 9, 12, 14, 16, 16, 15, 11, 8, 6]],
+  "Lleida" => [131, [7, 9, 10, 12, 15, 17, 20, 19, 17, 14, 10, 7]],
+  "Logrono" => [379, [7, 8, 10, 11, 13, 16, 18, 18, 16, 13, 10, 8]],
+  "Lugo" => [412, [7, 8, 9, 10, 11, 13, 15, 15, 14, 12, 9, 8]],
+  "Madrid" => [589, [8, 8, 10, 12, 14, 17, 20, 19, 17, 13, 10, 8]],
+  "Malaga" => [0, [12, 12, 13, 14, 16, 18, 20, 20, 19, 16, 14, 12]],
+  "Melilla" => [130, [12, 13, 13, 14, 16, 18, 20, 20, 19, 17, 14, 13]],
+  "Murcia" => [25, [11, 11, 12, 13, 15, 17, 19, 20, 18, 16, 13, 11]],
+  "Ourense" => [327, [8, 10, 11, 12, 14, 16, 18, 18, 17, 13, 11, 9]],
+  "Oviedo" => [214, [9, 9, 10, 10, 12, 14, 15, 16, 15, 13, 10, 9]],
+  "Palencia" => [722, [6, 7, 8, 10, 12, 15, 17, 17, 15, 12, 9, 6]],
+  "Palma_de_Mallorca" => [1, [11, 11, 12, 13, 15, 18, 20, 20, 19, 17, 14, 12]],
+  "Pamplona_Iruna" => [456, [7, 8, 9, 10, 12, 15, 17, 17, 16, 13, 9, 7]],
+  "Pontevedra" => [77, [10, 11, 11, 13, 14, 16, 17, 17, 16, 14, 12, 10]],
+  "Salamanca" => [770, [6, 7, 8, 10, 12, 15, 17, 17, 15, 12, 8, 6]],
+  "San_Sebastian" => [5, [9, 9, 10, 11, 12, 14, 16, 16, 15, 14, 11, 9]],
+  "Santa_Cruz_de_Tenerife" => [0, [15, 15, 16, 16, 17, 18, 20, 20, 20, 18, 17, 16]],
+  "Santander" => [1, [10, 10, 11, 11, 13, 15, 16, 16, 16, 14, 12, 10]],
+  "Segovia" => [1013, [6, 7, 8, 10, 12, 15, 18, 18, 15, 12, 8, 6]],
+  "Sevilla" => [9, [11, 11, 13, 14, 16, 19, 21, 21, 20, 16, 13, 11]],
+  "Soria" => [984, [5, 6, 7, 9, 11, 14, 17, 16, 14, 11, 8, 6]],
+  "Tarragona" => [1, [10, 11, 12, 14, 16, 18, 20, 20, 19, 16, 12, 11]],
+  "Teruel" => [995, [6, 7, 8, 10, 12, 15, 18, 17, 15, 12, 8, 6]],
+  "Toledo" => [445, [8, 9, 11, 12, 15, 18, 21, 20, 18, 14, 11, 8]],
+  "Valencia" => [8, [10, 11, 12, 13, 15, 17, 19, 20, 18, 16, 13, 11]],
+  "Valladolid" => [704, [6, 8, 9, 10, 12, 15, 18, 18, 16, 12, 9, 7]],
+  "Vitoria_Gasteiz" => [512, [7, 7, 8, 10, 12, 14, 16, 16, 14, 12, 8, 7]],
+  "Zamora" => [617, [6, 8, 9, 10, 13, 16, 18, 18, 16, 12, 9, 7]],
+  "Zaragoza" => [207, [8, 9, 10, 12, 15, 17, 20, 19, 17, 14, 10, 8]]
+}
 
 # Obtén la capital de provincia y altitud del clima dado
 # TODO: Lo ideal sería leer la provincia y la altitud del archivo de climas
@@ -78,10 +92,23 @@ def get_site_prov_alt(weather_file)
     altitud_emplazamiento = 114.0
   else
     zonaclimatica = weather_file[0, 2]
-    capital_prov, altitud_emplazamiento = CAPITAL_Y_ALT_REF_FOR_CLIMATEZONE[zonaclimatica]
+    capital_prov, altitud_emplazamiento = {
+      "A3" => ["Cadiz", 0],
+      "A4" => ["Almeria", 0],
+      "B3" => ["Valencia", 8],
+      "B4" => ["Sevilla", 9],
+      "C1" => ["Bilbao_Bilbo", 214],
+      "C2" => ["Barcelona", 1],
+      "C3" => ["Granada", 754],
+      "C4" => ["Toledo", 445],
+      "D1" => ["Vitoria_Gasteiz", 512],
+      "D2" => ["Zamora", 617],
+      "D3" => ["Madrid", 589],
+      "E1" => ["Burgos", 861]
+    }[zonaclimatica]
   end
 
-  return capital_prov, altitud_emplazamiento
+  [capital_prov, altitud_emplazamiento]
 end
 
 # Obtén diferencia de altitud con la capital y
@@ -89,12 +116,12 @@ end
 def get_water_temps(runner, weather_file)
   capital_prov, altitud_emplazamiento = get_site_prov_alt(weather_file)
 
-  temps_agua_red = cte_temps_map(runner)
-  if temps_agua_red.nil? || !temps_agua_red.key?(capital_prov)
+  altitud_capital, temps_agua_red = CTE_TEMPS_MAP[capital_prov]
+
+  if altitud_capital.nil? || temps_agua_red.nil?
     runner.registerError("Capital de provincia '#{capital_prov}' sin datos de temperatura de agua de red")
     return false
   end
-  altitud_capital, temps_agua_red = temps_agua_red[capital_prov]
 
   runner.registerValue("Capital de referencia AF", capital_prov)
   runner.registerInfo("Altitud de la capital de provincia: #{altitud_capital}")
