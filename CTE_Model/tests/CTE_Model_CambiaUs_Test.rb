acfd32e60115cf97b5acce9388e9922b6e0e3538
Rafael Villar Burke <pachi@ietcc.csic.es>
2024-06-07 16:53:41 +0200

    Simplifica todavía más el test

--- CTE_Model/tests/CTE_Model_CambiaUs_Test.rb
+++ CTE_Model/tests/CTE_Model_CambiaUs_Test.rb
@@ -47,23 +47,14 @@ def get_runner_model(file_path)
   [runner, model]
 end
 
-def get_surface(model, uuid)
+def get_surface_u(model, uuid)
   handle = OpenStudio.toUUID(uuid)
   objeto = model.getModelObject(handle).get
-
   if objeto.iddObject.name == "OS:Surface"
-    elem = objeto.to_Surface.get
-    construction = elem.construction.get
-    u = construction.thermalConductance.to_f
-  elsif objeto.iddObject.name == "OS:SubSurface"
-    elem = objeto.to_SubSurface.get
-    construction = elem.construction.get
-    u = construction.uFactor.to_f
-  else
-    puts("Error, #{objeto.iddObject.name}, uuid:#{uuid}")
+    objeto.to_Surface.get.construction.get.thermalConductance.to_f
+  else # objeto.iddObject.name == "OS:SubSurface"
+    objeto.to_SubSurface.get.construction.get.uFactor.to_f
   end
-
-  [elem, construction, u]
 end
 
 def get_solar_heat_gain_coefficient(model, uuid)
@@ -76,60 +67,29 @@ def get_solar_heat_gain_coefficient(model, uuid)
   construction.layers[0].to_SimpleGlazing.get.solarHeatGainCoefficient
 end
 
-def carga_elementos(model, elementos_para_test)
-  elementos = {}
-  elementos_para_test.each do |tipo, lista|
-    lista.each do |uuid|
-      _surface, _construction, u_inicial = get_surface(model, uuid)
-      elementos[uuid] = {"tipo" => tipo, "u_inicial" => u_inicial}
+# Localiza huecos exteriores soportados por la medida
+def find_windows(model)
+  model.getSpaces.flat_map do |space|
+    space.surfaces.flat_map do |surface|
+      next unless surface.outsideBoundaryCondition == "Outdoors" && surface.windExposure == "WindExposed"
+      surface.subSurfaces.map { |window| window.handle.to_s if ["FixedWindow", "OperableWindow", "GlassDoor", "Door", "OverheadDoor", "Skylight"].include?(window.subSurfaceType.to_s) }
     end
-  end
-  elementos
+  end.compact
 end
 
-def find_windows(model)
-  model.getSpaces.map { |s|
-    s.surfaces.map { |sf|
-      sf.subSurfaces.map { |w|
-        is_ok_boundary = sf.outsideBoundaryCondition == "Outdoors" && sf.windExposure == "WindExposed"
-        is_ok_type = ["FixedWindow", "OperableWindow", "GlassDoor", "Door", "OverheadDoor", "Skylight"].include?(w.subSurfaceType.to_s)
-        if is_ok_boundary && is_ok_type
-          w.handle.to_s
-        else
-          continue
-        end
-      }
-    }
-  }.flatten
+# Lista de opacos al aire o terreno que no son PT_ o _PT
+# outsideBoundaryCondition: "Outdoors", "Ground"
+# surfaceType: Wall, RoofCeiling, Floor
+def find_opaques(model)
+  model.getSpaces.flat_map do |space|
+    space.surfaces.map do |surface|
+      [surface.handle.to_s, surface.surfaceType.to_s, surface.outsideBoundaryCondition.to_s] if ["Outdoors", "Ground"].include?(surface.outsideBoundaryCondition) && !surface.name.to_s.upcase.include?("PT_") && !surface.name.to_s.upcase.include?("_PT")
+    end
+  end.compact
 end
 
 class CTE_CambiaUs_Test < MiniTest::Test
   TEST_DIR = Pathname.new(__dir__)
-
-  # Elementos de residencial.osm
-  ELEMENTOS_RESIDENCIAL_OSM = {
-    "muros_exteriores" => ["7aeba99e-3d64-4b69-a3b2-a7cf318f91f8", "1415c487-a7ec-493b-b6e2-40cb1770a6d7", "41d017a5-0490-4aa0-81bf-cd5b5e4a86c2", "b82786ae-b6c9-4cc1-ace2-e2c1771d17b2"],
-    # "muros_terrenos" => [],
-    # "muros_interiores" => [],
-    "cubiertas_exteriores" => ["7e5b0180-4940-4f82-8d55-a82b356ed256", "4dba6073-0b46-4a4e-88f5-9cf67b1c0b1e", "1b4202a7-389e-413c-a734-3903f64499f0", "c4875e32-9291-48b3-bfe0-94bad05eb0d5"],
-    "cubiertas_interiores" => ["bd6b3149-d34c-4ced-a718-7d9a506ca243", "d4a8d4b2-b59e-4e86-abe1-ba5a8fdc2431", "0355e260-8d73-4cd4-8b9c-f88882af2cad"],
-    # "suelos_exteriores" => [],
-    "suelos_interiores" => ["8c1e94b2-14cf-4a4c-9d46-907327050022", "50ded79a-3226-4fd9-98e9-e90968987d40", "57a5012c-8fa7-45a4-bd4e-05d1f80a1313"],
-    "suelos_terrenos" => ["21f60244-fb64-4abe-abc3-464182337e27", "1d1a27e5-c230-435a-ac39-f4210b362a6d", "134cfe13-6464-4e3f-8235-bc25a284eceb"],
-    "ventanas" => ["aedb937b-e035-4caf-8bd8-6d38aca58017", "bd75cc94-d2f4-4c61-89a0-ff79c0c406ba", "2f2adaad-60e6-4344-9d47-ee4787772d5c", "6852333b-05ee-4c91-a799-b7c1a0686274"],
-    "puertas" => ["089b1b05-c16f-46c8-acf6-461e97125a7f"],
-}.freeze
-
-  # Elementos de modelo R_N01_unif_adosada.osm
-  ELEMENTOS_R_N01_V23 = {
-    "muros_exteriores" => ["94b8d093-436a-4d00-a34f-04c863de0d08", "5fdc6f02-ab04-43f7-abbb-6e2f5b585420", "be553ff8-1374-4869-8bcf-30ffb53290f9", "1f85cdc1-bf9a-4bd9-a5b9-b99ce38cfb2a"],
-    # "puertas" => ["ce8352bc-f6a2-4fae-b36b-b57e2a1e235d", "1c40df56-8bb6-4450-bb4a-8e14fc6cf1c5"],
-    "muros_terrenos" => ["25e1b51d-94eb-4f8f-813b-6a41d6e5c876"],
-    "cubiertas_exteriores" => ["c0205929-9427-40b4-883e-34d52c6309cc", "9a0d5785-3883-499a-8c3f-c6a6a7c7ad12", "40464c22-46a4-4704-a5bc-db659410cd09"],
-    "suelos_terrenos" => ["21f60244-fb64-4abe-abc3-464182337e27"],
-    "suelos_exteriores" => ["31d16c6a-d398-49b1-bf40-b530d205037c", "f663dd7c-e24c-4fed-a937-61993c1095ba", "3ebf1a50-0485-485c-a1b2-bfa12c2026d7"],
-    "ventanas" => ["9971a391-9f3d-4035-b8c5-b6d182b46e33", "adb347c4-df6e-45a6-96fd-d8ac1969e1d3", "208eb93c-b12d-4ff8-ba6e-cd92428cd463", "2a1c0c1e-2aa8-459e-b57b-a217407912ad", "ce8352bc-f6a2-4fae-b36b-b57e2a1e235d"]
-  }.freeze
 
   # Patch to capture if --verbose was passed to minitest so we can call
   # show_output if so, and not do it otherwise
@@ -182,7 +142,7 @@ class CTE_CambiaUs_Test < MiniTest::Test
     find_windows(model).each do |uuid|
       g_final = get_solar_heat_gain_coefficient(model, uuid)
       assert_in_delta(args_hash["CTE_g_gl"], g_final, 0.001, "uuid -> #{uuid}")
-      _surface, _construction, u_final = get_surface(model, uuid)
+      u_final = get_surface_u(model, uuid)
       assert_in_delta(args_hash["CTE_U_huecos"], u_final, 0.001, "uuid -> #{uuid}")
     end
   end
@@ -212,7 +172,11 @@ class CTE_CambiaUs_Test < MiniTest::Test
       argument_map[arg.name] = temp_arg_var
     end
 
-    elementos = carga_elementos(model, ELEMENTOS_R_N01_V23)
+    initial_values = {}
+    find_opaques(model).each do |uuid, _type, _boundary|
+      u = get_surface_u(model, uuid)
+      initial_values[uuid] = u
+    end
 
     measure.run(model, runner, argument_map)
     result = runner.result
@@ -223,13 +187,13 @@ class CTE_CambiaUs_Test < MiniTest::Test
     # Asserts de condiciones
     assert_equal("Success", result.value.valueName)
 
-    elementos.each do |uuid, atributos|
-      _surface, _construction, u_final = get_surface(model, uuid)
-      assert_in_delta(atributos["u_inicial"], u_final, 0.001, "uuid -> #{uuid}")
+    initial_values.each do |uuid, u_inicial|
+      u_final = get_surface_u(model, uuid)
+      assert_in_delta(u_inicial, u_final, 0.001, "uuid -> #{uuid}")
     end
   end
 
-  def test_cambia_u
+  def test_cambia_u_opacos
     # create an instance of the measure
     measure = CTE_Model.new
 
@@ -255,25 +219,24 @@ class CTE_CambiaUs_Test < MiniTest::Test
       argument_map[arg.name] = temp_arg_var
     end
 
-    elementos = carga_elementos(model, ELEMENTOS_R_N01_V23)
-
     measure.run(model, runner, argument_map)
     result = runner.result
 
     assert_equal("Success", result.value.valueName)
 
+    # puts(find_opaques(model))
+
     transmitancias = {
-      "muros_exteriores" => args_hash["CTE_U_muros"],
-      "muros_terrenos" => 1 / (1 / args_hash["CTE_U_muros"] - 0.5),
-      "cubiertas_exteriores" => args_hash["CTE_U_cubiertas"],
-      "suelos_terrenos" => 1 / (1 / args_hash["CTE_U_suelos"] - 0.5),
-      "suelos_exteriores" => args_hash["CTE_U_suelos"],
-      "ventanas" => args_hash["CTE_U_huecos"]
+      ["Wall", "Outdoors"] => args_hash["CTE_U_muros"],
+      ["Wall", "Ground"] => 1 / (1 / args_hash["CTE_U_muros"] - 0.5),
+      ["RoofCeiling", "Outdoors"] => args_hash["CTE_U_cubiertas"],
+      ["Floor", "Ground"] => 1 / (1 / args_hash["CTE_U_suelos"] - 0.5),
+      ["Floor", "Outdoors"] => args_hash["CTE_U_suelos"],
     }
 
-    elementos.each do |uuid, atributos|
-      _surface, _construction, u_final = get_surface(model, uuid)
-      assert_in_delta(u_final, transmitancias[atributos["tipo"]], 0.01, "uuid -> #{uuid}")
+    find_opaques(model).each do |uuid, type, boundary|
+      u_final = get_surface_u(model, uuid)
+      assert_in_delta(u_final, transmitancias[[type, boundary]], 0.01, "uuid -> #{uuid}")
     end
   end
 end
